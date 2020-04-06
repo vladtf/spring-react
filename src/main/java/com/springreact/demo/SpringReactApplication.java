b5609b92d83b852e1527c0a2c21ab8af004a3da7
vladtf <vendeta0263@gmail.com>
2020-04-06 03:01:58 +0300

    Initial commit

---
+++ src/main/java/com/springreact/demo/SpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.springreact.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactApplication.class, args);
+    }
+
+}
