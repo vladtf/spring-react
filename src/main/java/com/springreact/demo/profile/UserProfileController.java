260b0db7b4bd72f0fa21c44285264f2126296d7d
vladtf <vendeta0263@gmail.com>
2020-04-06 21:47:37 +0300

    Add UserProfileDataAccessService and UserProfileService to wire-up database and controller Fixed AmazonConfig region issue

--- src/main/java/com/springreact/demo/profile/UserProfileController.java
+++ src/main/java/com/springreact/demo/profile/UserProfileController.java
@@ -1,11 +1,12 @@
 package com.springreact.demo.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -19,7 +20,17 @@ public class UserProfileController {
     }
 
     @GetMapping
-    public List<UserProfile> getUserProfiles(){
+    public List<UserProfile> getUserProfiles() {
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file") MultipartFile file) {
+            userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
--- src/main/java/com/springreact/demo/profile/UserProfileService.java
+++ src/main/java/com/springreact/demo/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.springreact.demo.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -17,5 +19,9 @@ public class UserProfileService {
 
     public List<UserProfile> getUserProfiles(){
         return  dataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+
     }
 }
