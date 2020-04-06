a085eebb5844c55284b527b0e902897ad446d0ea
vladtf <vendeta0263@gmail.com>
2020-04-07 02:54:17 +0300

    Implemented axios to link with backend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+
+const UserProfiles = () => {
+    const fetchUserProfile = () => {
+        axios.get("http://localhost:8080/magement/api/v1/user").then(res => {
+            console.log(res);
+        });
+    };
+
+    useEffect(() => {
+        fetchUserProfile();
+    }, []);
+
+    return <h1>Hello</h1>
+};
+
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <UserProfiles/>
         </div>
     );
 }
