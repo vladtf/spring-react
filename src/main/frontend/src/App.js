f1aa60b1028b8180e9e10163b6d169e408d12aea
vladtf <vendeta0263@gmail.com>
2020-04-08 00:10:50 +0300

    Add method to send images to backend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -22,17 +22,39 @@ const UserProfiles = () => {
 
         return (
             <div key={index}>
-                <MyDropzone/>
+                <br/>
+                <br/>
                 <h1>{userProfile.username}</h1>
                 <p>{userProfile.userProfileId}</p>
+                <MyDropzone {...userProfile} />
+                <br/>
             </div>
         )
     })
 };
 
-function MyDropzone() {
+function MyDropzone(userProfileId) {
     const onDrop = useCallback(acceptedFiles => {
-        // Do something with the files
+        const file = acceptedFiles[0];
+
+        console.log(file);
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        axios.post(`http://localhost:8080/api/v1/user/${userProfileId}/image/upload`,
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }
+        ).then(() => {
+            console.log("File uploaded successfully")
+        }).catch(err => {
+            console.log(err);
+        });
+
     }, [])
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
@@ -41,8 +63,8 @@ function MyDropzone() {
             <input {...getInputProps()} />
             {
                 isDragActive ?
-                    <p>Drop the files here ...</p> :
-                    <p>Drag 'n' drop some files here, or click to select files</p>
+                    <p>Drop the image ...</p> :
+                    <p>Drag 'n' drop profile image, or click to select profile image</p>
             }
         </div>
     )
