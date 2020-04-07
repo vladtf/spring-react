04d072a2f51dfa5fdc6fa371600552493fc9280c
vladtf <vendeta0263@gmail.com>
2020-04-07 18:14:01 +0300

    Add react-dropzone to send images

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
+import {useDropzone} from 'react-dropzone'
 
 const UserProfiles = () => {
 
@@ -21,12 +22,31 @@ const UserProfiles = () => {
 
         return (
             <div key={index}>
+                <MyDropzone/>
                 <h1>{userProfile.username}</h1>
                 <p>{userProfile.userProfileId}</p>
             </div>
         )
     })
 };
+
+function MyDropzone() {
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+        </div>
+    )
+}
 
 
 function App() {
--- src/main/java/com/springreact/demo/profile/UserProfileService.java
+++ src/main/java/com/springreact/demo/profile/UserProfileService.java
@@ -17,11 +17,15 @@ public class UserProfileService {
         this.dataAccessService = dataAccessService;
     }
 
-    public List<UserProfile> getUserProfiles(){
-        return  dataAccessService.getUserProfiles();
+    public List<UserProfile> getUserProfiles() {
+        return dataAccessService.getUserProfiles();
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
-
+        // 1. Check if image is not empty
+        // 2. If file is an image
+        // 3. Whether the user exists in database
+        // 4. Grab some metadata from file if any
+        // 5. Store the image in S3 and update database (userProfileImageLing) with s3 image link
     }
 }
